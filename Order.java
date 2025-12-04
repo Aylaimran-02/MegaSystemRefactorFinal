02b5dc1e3de3e8725237ab23f7e4e844689ec6e2
Ayla Imran <104843244+Aylaimran-02@users.noreply.github.com>
2025-12-04 17:44:50 +0500

    Create Order class with order details fields

---
+++ Order.java
@@ -0,0 +1,13 @@
+import java.time.LocalDate;
+
+public class Order {
+
+    public String userName;
+    public int productId;
+    public int quantity;
+    public double price;
+    public String address;
+    public boolean urgent;
+    public String notes;
+    public LocalDate date;
+}
